0be25a851cfe69a6bf423ada502271cca65a03eb
Dang Giap <giap100404@gmail.com>
2025-10-24 12:21:20 +0700

    24/10/2025

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import BackgroundSection from '../components/BackgroundSection'
+
+
+const Home = () => {
+    return (
+        <>
+            <BackgroundSection />
+        </>
+    )
+}
+
+export default Home
